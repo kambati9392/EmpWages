c90be6497c1f2c7533f7ea73cc4e476cb8418356
Rammohan <reddy.kambati@gmail.com>
2024-09-11 10:30:09 +0530

    ADDED functionality of EmpAtt

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,5 +3,18 @@ package com.bridgelabz.employeewage;
 public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+        if(empCheck == 0)
+        {
+            System.out.println("Employee is Present");
+        }
+        else
+        {
+            System.out.println("Employee is Absent");
+        }
+
+    }
+	
+	
 	}
 }
